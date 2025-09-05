e1ebd96b469d6e9402f35eeb57b3989075ea6a17
gmarquette <gab64320@gmail.com>
2025-09-05 14:51:02 +0200

    Adding Return Button

--- GuessWhoOnePiece/Components/Pages/CharacterInfo.razor.cs
+++ GuessWhoOnePiece/Components/Pages/CharacterInfo.razor.cs
@@ -3,6 +3,8 @@
 // </copyright>
 // <author>Gabriel Marquette</author>
 
+using GuessWhoOnePiece.Components.Layout;
+using GuessWhoOnePiece.Model;
 using GuessWhoOnePiece.Model.Characters;
 using Microsoft.AspNetCore.Components;
 
@@ -11,9 +13,16 @@ namespace GuessWhoOnePiece.Components.Pages;
 public partial class CharacterInfo : ComponentBase
 {
     [Parameter] public Character? Character {get; set; }
+    [CascadingParameter(Name = "MainLayout")] private MainLayout? MainLayout { get; set; }
 
     protected override void OnParametersSet()
     {
         Character = CurrentCharacterService.CurrentCharacter;
+    }
+
+    private void OnBack()
+    {
+        MainLayout!.TabBarRef.ChangeActiveState(IndexMenuTabBar.Characters);
+        Navigation.NavigateTo("/characters");
     }
 }
